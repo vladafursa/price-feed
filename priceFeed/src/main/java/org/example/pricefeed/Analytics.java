8ac0525170cf1e14a99b562998d67e77089e11ac
Vlada Fursa <fursa_vlada@mail.ru>
2026-08-09 00:14:49 +0100

    added price generator class

---
+++ priceFeed/src/main/java/org/example/pricefeed/Analytics.java
@@ -0,0 +1,4 @@
+package org.example.pricefeed;
+
+public class Analytics {
+}
---
+++ priceFeed/src/main/java/org/example/pricefeed/PriceGenerator.java
@@ -0,0 +1,12 @@
+package org.example.pricefeed;
+
+import java.math.BigDecimal;
+
+public class PriceGenerator {
+
+    public BigDecimal generatePrice(BigDecimal currentPrice){
+        double changePercent = (Math.random() - 0.5) * 0.02;
+        return currentPrice.multiply(BigDecimal.valueOf(1 + changePercent));
+    }
+
+}
