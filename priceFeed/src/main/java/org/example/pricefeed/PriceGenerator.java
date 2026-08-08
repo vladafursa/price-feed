3cc156f58f59dbdf1264d9046150dedcae03f039
Vlada Fursa <fursa_vlada@mail.ru>
2026-08-09 00:20:48 +0100

    added static keyword

--- priceFeed/src/main/java/org/example/pricefeed/PriceGenerator.java
+++ priceFeed/src/main/java/org/example/pricefeed/PriceGenerator.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public class PriceGenerator {
 
-    public BigDecimal generatePrice(BigDecimal currentPrice){
+    public static BigDecimal generatePrice(BigDecimal currentPrice){
         double changePercent = (Math.random() - 0.5) * 0.02;
         return currentPrice.multiply(BigDecimal.valueOf(1 + changePercent));
     }
