5a433e9b3cffe151cd75c5be51076723f09a35fe
Vlada Fursa <fursa_vlada@mail.ru>
2026-08-09 00:33:10 +0100

    added prices engine class

--- priceFeed/src/main/java/org/example/pricefeed/PriceEngine.java
+++ priceFeed/src/main/java/org/example/pricefeed/PriceEngine.java
@@ -1,4 +1,33 @@
 package org.example.pricefeed;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class PriceEngine {
+    ConcurrentHashMap<String, FinancialInstrument> instruments;
+    PriceHistory history;
+
+    public void updateSpecificPrice(String symbol){
+        FinancialInstrument instrument = instruments.get(symbol);
+        if (instrument != null) {
+            updatePrice(instrument);
+        }
+    }
+
+    public void updateAllPrices(){
+        for(FinancialInstrument instrument : instruments.values()){
+            updatePrice(instrument);
+        }
+    }
+
+    public void updatePrice(FinancialInstrument instrument){
+        BigDecimal newPrice = PriceGenerator.generatePrice(instrument.getPrice());
+        instrument.setPrice(newPrice);
+        history.addPrice(instrument, newPrice);
+    }
+
+    public void addInstrument(FinancialInstrument instrument){
+        instruments.put(instrument.getName(), instrument);
+    }
 }
