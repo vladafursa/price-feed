6349f12f2844438ecacfc397f57590af9f63b43d
Vlada Fursa <fursa_vlada@mail.ru>
2026-08-09 01:28:41 +0100

    added rounding mode

--- priceFeed/src/main/java/org/example/pricefeed/PriceGenerator.java
+++ priceFeed/src/main/java/org/example/pricefeed/PriceGenerator.java
@@ -1,12 +1,15 @@
 package org.example.pricefeed;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class PriceGenerator {
 
     public static BigDecimal generatePrice(BigDecimal currentPrice){
         double changePercent = (Math.random() - 0.5) * 0.02;
-        return currentPrice.multiply(BigDecimal.valueOf(1 + changePercent));
+        return currentPrice
+                .multiply(BigDecimal.valueOf(1 + changePercent))
+                .setScale(2, RoundingMode.HALF_UP);
     }
 
 }
